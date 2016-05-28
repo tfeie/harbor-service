1b35cc7779e8fff07e24d7ef109b167e4ac31065
zhangchao2 <zhangchao2@asiainfo.com>
2016-05-28 23:04:50 +0800

    新增服务启动测试

---
+++ harbor-core/src/test/java/com/the/harbor/test/DubboTest.java
@@ -0,0 +1,11 @@
+package com.the.harbor.test;
+
+import com.the.harbor.commons.appserver.DubboServiceStart;
+
+public class DubboTest {
+
+    public static void main(String[] args) {
+        DubboServiceStart.main(args);
+    }
+
+}
