bc92b3c2e1d7f508403fa9fff83ccb695e5092eb
zhangchao2 <zhangchao2@asiainfo.com>
2016-06-06 11:05:12 +0800

    1

---
+++ harbor-core/src/main/java/com/the/harbor/dao/mapper/bo/HyTags.java
@@ -0,0 +1,33 @@
+package com.the.harbor.dao.mapper.bo;
+
+public class HyTags {
+    private String tagId;
+
+    private String tagType;
+
+    private String tagName;
+
+    public String getTagId() {
+        return tagId;
+    }
+
+    public void setTagId(String tagId) {
+        this.tagId = tagId == null ? null : tagId.trim();
+    }
+
+    public String getTagType() {
+        return tagType;
+    }
+
+    public void setTagType(String tagType) {
+        this.tagType = tagType == null ? null : tagType.trim();
+    }
+
+    public String getTagName() {
+        return tagName;
+    }
+
+    public void setTagName(String tagName) {
+        this.tagName = tagName == null ? null : tagName.trim();
+    }
+}
