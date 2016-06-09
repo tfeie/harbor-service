434ecbc5a78aff1b6c38ddb7027d83d3b7129457
zhangchao2 <zhangchao2@asiainfo.com>
2016-06-09 15:59:17 +0800

    修改认证服务提示信息

--- harbor-api/src/main/java/com/the/harbor/api/user/param/UserCertificationReq.java
+++ harbor-api/src/main/java/com/the/harbor/api/user/param/UserCertificationReq.java
@@ -13,19 +13,19 @@ public class UserCertificationReq implements Serializable {
 	/**
 	 * 用户标识
 	 */
-	@NotBlank(message = "英文名不能为空", groups = { IUserSV.SubmitUserCertification.class })
+	@NotBlank(message = "用户不能为空", groups = { IUserSV.SubmitUserCertification.class })
 	private String userId;
 
 	/**
 	 * 身份证照片地址
 	 */
-	@NotBlank(message = "英文名不能为空", groups = { IUserSV.SubmitUserCertification.class })
+	@NotBlank(message = "请上传身份证正面照", groups = { IUserSV.SubmitUserCertification.class })
 	private String idcardPhoto;
 
 	/**
 	 * 海外证件照片地址
 	 */
-	@NotBlank(message = "英文名不能为空", groups = { IUserSV.SubmitUserCertification.class })
+	@NotBlank(message = "请上传海外学历认证/签证/学生证", groups = { IUserSV.SubmitUserCertification.class })
 	private String overseasPhoto;
 
 	public String getUserId() {
