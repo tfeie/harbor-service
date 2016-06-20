6f98b672a1eea114cb3f272a21a7ccdab59e5416
zhangchao2 <zhangchao2@asiainfo.com>
2016-06-20 21:31:23 +0800

    1

--- harbor-api/src/main/java/com/the/harbor/api/go/IGoSV.java
+++ harbor-api/src/main/java/com/the/harbor/api/go/IGoSV.java
@@ -2,14 +2,38 @@ package com.the.harbor.api.go;
 
 import javax.validation.constraints.NotNull;
 
+import com.the.harbor.api.go.param.CreateGoPaymentOrderReq;
+import com.the.harbor.api.go.param.CreateGoPaymentOrderResp;
 import com.the.harbor.api.go.param.GoCreateReq;
 import com.the.harbor.api.go.param.GoCreateResp;
+import com.the.harbor.api.go.param.GoOrderCheckReq;
+import com.the.harbor.api.go.param.GoOrderCheckResp;
+import com.the.harbor.api.go.param.GoOrderCreateReq;
+import com.the.harbor.api.go.param.GoOrderCreateResp;
+import com.the.harbor.api.go.param.GoOrderQueryReq;
+import com.the.harbor.api.go.param.GoOrderQueryResp;
 import com.the.harbor.base.exception.BusinessException;
 import com.the.harbor.base.exception.SystemException;
 
 public interface IGoSV {
 
 	@interface CreateGo {
+
+	}
+
+	@interface CheckUserJoinGo {
+
+	}
+
+	@interface OrderOneOnOne {
+
+	}
+
+	@interface CreateGoPaymentOrder {
+
+	}
+
+	@interface QueryGoOrderDetail {
 
 	}
 
@@ -22,5 +46,50 @@ public interface IGoSV {
 	 * @throws SystemException
 	 */
 	GoCreateResp createGo(@NotNull(message = "参数为空") GoCreateReq goCreateReq) throws BusinessException, SystemException;
+
+	/**
+	 * 校验用户是否参与了某个活动
+	 * 
+	 * @param goOrderCheckReq
+	 * @return
+	 * @throws BusinessException
+	 * @throws SystemException
+	 */
+	GoOrderCheckResp checkUserJoinGo(@NotNull(message = "参数为空") GoOrderCheckReq goOrderCheckReq)
+			throws BusinessException, SystemException;
+
+	/**
+	 * 预约一个OneOnOne的活动
+	 * 
+	 * @param goOrderCreateReq
+	 * @return
+	 * @throws BusinessException
+	 * @throws SystemException
+	 */
+	GoOrderCreateResp orderOneOnOne(@NotNull(message = "参数为空") GoOrderCreateReq goOrderCreateReq)
+			throws BusinessException, SystemException;
+
+	/**
+	 * 产生一个OneOnOne活动的支付流水
+	 * 
+	 * @param createGoPaymentOrderReq
+	 * @return
+	 * @throws BusinessException
+	 * @throws SystemException
+	 */
+	CreateGoPaymentOrderResp createGoPaymentOrder(
+			@NotNull(message = "参数为空") CreateGoPaymentOrderReq createGoPaymentOrderReq)
+			throws BusinessException, SystemException;
+
+	/**
+	 * 查询One on One活动预约信息
+	 * 
+	 * @param goOrderQueryReq
+	 * @return
+	 * @throws BusinessException
+	 * @throws SystemException
+	 */
+	GoOrderQueryResp queryGoOrderDetail(@NotNull(message = "参数为空") GoOrderQueryReq goOrderQueryReq)
+			throws BusinessException, SystemException;
 
 }
