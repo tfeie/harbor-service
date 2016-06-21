93e09b4f0e733c44f1003e64a441cf20c325a3bb
zhangchao2 <zhangchao2@asiainfo.com>
2016-06-21 09:07:24 +0800

    1

--- harbor-core/src/main/java/com/the/harbor/service/impl/GoBusiSVImpl.java
+++ harbor-core/src/main/java/com/the/harbor/service/impl/GoBusiSVImpl.java
@@ -161,8 +161,8 @@ public class GoBusiSVImpl implements IGoBusiSV {
 			throw new BusinessException("GO_0001", "产生支付交易流水失败:预约记录不存在");
 		}
 		if (!StringUtil.isBlank(goOrder.getPayOrderId())) {
-			if (!OrderStatus.WAIT_PAY.equals(goOrder.getOrderStatus())
-					|| !OrderStatus.PAY_FAILURE.equals(goOrder.getOrderStatus())) {
+			if (!OrderStatus.WAIT_PAY.getValue().equals(goOrder.getOrderStatus())
+					|| !OrderStatus.PAY_FAILURE.getValue().equals(goOrder.getOrderStatus())) {
 				throw new BusinessException("GO_0001", "此活动预约记录已经发起一笔支付交易[" + goOrder.getPayOrderId() + "]");
 			}
 			payOrderId = goOrder.getPayOrderId();
