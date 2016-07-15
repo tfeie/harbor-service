b140f44c15da058422d9b555a301095cb4f8634f
choaryzhang <choaryzhang@163.com>
2016-07-15 14:19:42 +0800

    1

--- harbor-core/src/test/java/com/the/harbor/test/NotifyMQTest.java
+++ harbor-core/src/test/java/com/the/harbor/test/NotifyMQTest.java
@@ -3,6 +3,7 @@ package com.the.harbor.test;
 import com.aliyun.mns.client.CloudAccount;
 import com.aliyun.mns.client.MNSClient;
 import com.aliyun.mns.model.Message;
+import com.the.harbor.commons.appserver.MQListenerStart;
 import com.the.harbor.commons.components.aliyuncs.mns.MNSSettings;
 import com.the.harbor.commons.components.aliyuncs.mns.MessageReceiver;
 import com.the.harbor.commons.components.globalconfig.GlobalSettings;
@@ -11,28 +12,7 @@ import com.the.harbor.commons.util.StringUtil;
 public class NotifyMQTest {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		CloudAccount account = new CloudAccount(MNSSettings.getMNSAccessKeyId(), MNSSettings.getMNSAccessKeySecret(),
-				MNSSettings.getMNSAccountEndpoint());
-		MNSClient sMNSClient = account.getMNSClient();
-		MessageReceiver receiver = new MessageReceiver(1, sMNSClient, GlobalSettings.getNotifyQueueName());
-		while (true) {
-			Message message = receiver.receiveMessage();
-			boolean success = false;
-			try {
-				if (!StringUtil.isBlank(message.getMessageBody())) {
-					System.out.println(message.getMessageBody());
-				}
-				success = true;
-			} catch (Exception ex) {
-				success = false;
-			}
-			if (success) {
-				// 如果记录入库成功，则需要从MNS删除消息
-				sMNSClient.getQueueRef(GlobalSettings.getNotifyQueueName()).deleteMessage(message.getReceiptHandle());
-			}
-		}
-
+		MQListenerStart.main(args);
 	}
 
 }
