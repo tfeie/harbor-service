de4ad073bbc87960d9599d24e537d3e171ed600d
zhangchao2 <zhangchao2@asiainfo.com>
2016-08-19 15:33:21 +0800

    GROUP与ONO活动支付成功后，通知活动发起者

--- harbor-core/src/main/java/com/the/harbor/service/impl/PaymentBusiSVImpl.java
+++ harbor-core/src/main/java/com/the/harbor/service/impl/PaymentBusiSVImpl.java
@@ -1,20 +1,33 @@
 package com.the.harbor.service.impl;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.alibaba.fastjson.JSONObject;
 import com.the.harbor.api.go.param.UpdateGoJoinPayReq;
 import com.the.harbor.api.go.param.UpdateGoOrderPayReq;
 import com.the.harbor.api.pay.param.CreatePaymentOrderReq;
 import com.the.harbor.api.pay.param.NotifyPaymentReq;
 import com.the.harbor.api.user.param.DoUserAssetsTrade;
+import com.the.harbor.api.user.param.UserViewInfo;
 import com.the.harbor.base.enumeration.hypaymentorder.BusiType;
 import com.the.harbor.base.enumeration.hypaymentorder.PayStatus;
 import com.the.harbor.base.enumeration.hyuser.SystemUser;
 import com.the.harbor.base.enumeration.hyuserassets.AssetsType;
 import com.the.harbor.base.exception.BusinessException;
+import com.the.harbor.commons.components.aliyuncs.sms.SMSSender;
+import com.the.harbor.commons.components.aliyuncs.sms.param.SMSSendRequest;
+import com.the.harbor.commons.components.globalconfig.GlobalSettings;
+import com.the.harbor.commons.redisdata.util.HyCfgUtil;
 import com.the.harbor.commons.util.DateUtil;
+import com.the.harbor.commons.util.StringUtil;
+import com.the.harbor.dao.mapper.bo.HyGo;
+import com.the.harbor.dao.mapper.bo.HyGoJoin;
+import com.the.harbor.dao.mapper.bo.HyGoOrder;
 import com.the.harbor.dao.mapper.bo.HyPaymentOrder;
 import com.the.harbor.dao.mapper.bo.HyUserBuyHb;
 import com.the.harbor.dao.mapper.bo.HyUserBuyMember;
@@ -104,6 +117,27 @@ public class PaymentBusiSVImpl implements IPaymentBusiSV {
 			updateGoJoinPayReq.setPayOrderId(payOrder.getPayOrderId());
 			updateGoJoinPayReq.setPayStatus("SUCCESS");
 			goBusiSV.updateGoJoinPay(updateGoJoinPayReq);
+			
+			//GROUP活动支付成功通知发起者
+			HyGoJoin hyGoJoin = goBusiSV.getHyGoJoin(payOrder.getSourceNo());
+			if (hyGoJoin != null) {
+				// 获取活动信息
+				HyGo hyGo = goBusiSV.getHyGo(hyGoJoin.getGoId());
+				UserViewInfo userInfo = userManagerSV.getUserViewInfoByUserId(hyGo.getUserId());
+				if (userInfo != null && !StringUtil.isBlank(userInfo.getMobilePhone())) {
+					SMSSendRequest req = new SMSSendRequest();
+					List<String> recNumbers = new ArrayList<String>();
+					recNumbers.add(userInfo.getMobilePhone());
+					JSONObject smsParams = new JSONObject();
+					smsParams.put("goTopic", hyGo.getTopic());
+					req.setRecNumbers(recNumbers);
+					req.setSmsFreeSignName(GlobalSettings.getSMSFreeSignName());
+					req.setSmsParams(smsParams);
+					req.setSmsTemplateCode(HyCfgUtil.getSMSCodeOfOnOApplied());
+					SMSSender.send(req);
+				}
+			}
+			
 		} else if (BusiType.PAY_FOR_HAIBI.getValue().equals(busiType)) {
 			// 充值海贝，需要将海贝冲入账户
 			String buyOrderId = payOrder.getSourceNo();
@@ -129,13 +163,32 @@ public class PaymentBusiSVImpl implements IPaymentBusiSV {
 
 			}
 		} else if (BusiType.PAY_FOR_ONO.getValue().equals(busiType)) {
-			//ono活动支付成功处理
+			// ono活动支付成功处理
 			UpdateGoOrderPayReq updateGoOrderPayReq = new UpdateGoOrderPayReq();
 			updateGoOrderPayReq.setGoOrderId(payOrder.getSourceNo());
 			updateGoOrderPayReq.setPayOrderId(payOrder.getPayOrderId());
 			updateGoOrderPayReq.setPayStatus("SUCCESS");
 			goBusiSV.updateGoOrderPay(updateGoOrderPayReq);
-			
+
+			// 支付成功后，给活动发起方发生短信提醒
+			HyGoOrder hyGoOrder = goBusiSV.getHyGoOrder(payOrder.getSourceNo());
+			if (hyGoOrder != null) {
+				// 获取活动信息
+				HyGo hyGo = goBusiSV.getHyGo(hyGoOrder.getGoId());
+				UserViewInfo userInfo = userManagerSV.getUserViewInfoByUserId(hyGo.getUserId());
+				if (userInfo != null && !StringUtil.isBlank(userInfo.getMobilePhone())) {
+					SMSSendRequest req = new SMSSendRequest();
+					List<String> recNumbers = new ArrayList<String>();
+					recNumbers.add(userInfo.getMobilePhone());
+					JSONObject smsParams = new JSONObject();
+					smsParams.put("goTopic", hyGo.getTopic());
+					req.setRecNumbers(recNumbers);
+					req.setSmsFreeSignName(GlobalSettings.getSMSFreeSignName());
+					req.setSmsParams(smsParams);
+					req.setSmsTemplateCode(HyCfgUtil.getSMSCodeOfOnOApplied());
+					SMSSender.send(req);
+				}
+			}
 		}
 	}
 
